ca2d9cb8aaaa6cd6937e0df911c5bac0a9864e5c
LiorJerbi <lior.Jerbi@msmail.Ariel.ac.il>
2022-12-07 22:42:28 +0200

    final commit, after comments

--- main.c
+++ main.c
@@ -4,30 +4,30 @@
 
 
 int main(){
-    int mat[10][10] = {{0}}; //constructor of the matrix and initailizing to 0 all indexes.
+    int mat[10][10] = {{0}}; /*constructor of the matrix and initailizing to 0 all indexes.*/
     char l = '0';  
-    while(l != 'D'){  //when the user press D program stops.
-        scanf("%c ",&l); //the user enter a letter to decide which function to run.
-        if(l == 'A'){ //when the user press A enter here.
+    while(l != 'D'){  /*when the user press D program stops.*/
+        scanf("%c ",&l); /*the user enter a letter to decide which function to run.*/
+        if(l == 'A'){ /*when the user press A enter here.*/
             int x,y;
-            for(x=0; x<10 ; x++){ //fills the matrix with values.
+            for(x=0; x<10 ; x++){ /*fills the matrix with values.*/
                 for(y=0; y<10; y++){
                     scanf("%d", &mat[x][y]);
                 } 
             }
-           getMatrix(mat);  //Doing Floyd-Warshall Algorithem on the matrix.
+           getMatrix(mat);  /*Doing Floyd-Warshall Algorithem on the matrix.*/
         }
-        else if( l == 'B'){ //when the user press B enter here.
+        else if( l == 'B'){ /*when the user press B enter here.*/
             int i ,j;
             scanf("%d",&i);
             scanf(" %d",&j);
-            isthereRoute(mat,i,j); //user enter 2 vertices to find if theres a route between them.
+            isthereRoute(mat,i,j); /*user enter 2 vertices to find if theres a route between them.*/
         }
-        else if(l == 'C'){ //when the user press C enter here.
+        else if(l == 'C'){ /*when the user press C enter here.*/
              int i ,j;
             scanf("%d",&i);
             scanf(" %d",&j);
-            theShortestRoute(mat,i,j); //user enter 2 vertices to find the weight of the shortest route between them.
+            theShortestRoute(mat,i,j); /*user enter 2 vertices to find the weight of the shortest route between them.*/
 
        }
     }
